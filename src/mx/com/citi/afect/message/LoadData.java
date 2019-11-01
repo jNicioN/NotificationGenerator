2a24dc6347ae751cea88ed9683fee97a815cd2e3
Jonathan Nicio <jnicion@gmail.com>
2019-11-01 17:16:36 -0600

    feat: Add an ID parameter

--- src/mx/com/citi/afect/message/LoadData.java
+++ src/mx/com/citi/afect/message/LoadData.java
@@ -4,6 +4,7 @@ public class LoadData {
 	private int typeNotif;
 	private String imei;
 	private String delay;
+	private String notificationId;
 
 	public void setTypeNotif(int typeNotif) {
 		this.typeNotif = typeNotif;
@@ -17,17 +18,37 @@ public class LoadData {
 		this.delay = delay;
 	}
 
+	public void setNotificationId(String notificationId ) {
+		this.notificationId = notificationId;
+	}
+
 	public String getNotification() {
 		String strMessage;
 
 		NotificationSelector message = new NotificationSelector();
 		strMessage = message.getMessage(typeNotif);
 
+		strMessage = replaceSpecialCharacters(strMessage);
+
+
 		strMessage = strMessage.replace("IMEIVALUE", imei);
 		strMessage = strMessage.replace("EXTIME", delay);
+		strMessage = strMessage.replace("NOTIDVALUE", notificationId );
 
 		System.out.println(strMessage);
 
 		return strMessage;
 	}
+
+	private static String replaceSpecialCharacters(String chain){
+		String msg;
+		msg = chain.replace("\r", "\\n")
+				.replace("\\r", "\\n")
+				.replace("\n", "\\n")
+				.replace("\f", "\\f")
+				.replace("\t", "\\t");
+		return msg;
+	}
+
+
 }
